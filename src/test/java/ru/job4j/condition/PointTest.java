28c7349e06c0bc16e450c88499e7d639390a4e38
Alexander Zukov <linuzzz7@yandex.ru>
2020-07-09 10:29:12 +0300

    3. Перегрузить метод max для трех чисел [#322160]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double expected = 2.0;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(0, 0, 3);
+        Point b = new Point(0, 2, 6);
+        double expected = 2.0;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
