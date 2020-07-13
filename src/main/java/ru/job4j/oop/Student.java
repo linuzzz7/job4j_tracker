a84ba9380744813fc569fd04f0cf29847ea6deb6
Alexander Zukov <linuzzz7@yandex.ru>
2020-07-13 14:08:41 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#322131]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,19 +1,5 @@
 package ru.job4j.oop;
 
-public class Student {
+public class Student extends Object {
 
-    public void music(String lyrics) {
-        System.out.println("I can sign a song : " + lyrics);
-    }
-
-    public void song() {
-        System.out.println("I believe I can fly");
-    }
-
-    public static void main(String[] args) {
-        Student petya = new Student();
-        String song = "I believe, I can fly";
-        petya.music(song);
-
-    }
 }
